c065da711a6d878346a9a9410823385ee753fbf2
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-25 20:37:26 -0700

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,17 @@
+# This will add two integers given by user
+
+print("Enter first number")
+s1 = input()
+n1 = int(s1)
+
+print("Enter second number")
+s2 = input()
+n2 = int(s2)
+
+res = n1 + n2
+print(res)
+print("res =",res)
+
+#print()  -->for output purpose
+#input()  -->for accepting input from user --> string
+#int()    -->for converting string into integer
