fb113fbd2f4c2cbd998376f223a5692ef43d50fc
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-25 20:27:35 -0700

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,7 @@
+#This program will add two numbers
+
+n1 = 10               #Dynamically typed language(no need to write datatype)
+n2 = 25
+result = n1 + n2
+print(result)
+print("result =", result)
