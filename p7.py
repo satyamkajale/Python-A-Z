ee75daac748704a099bd78a1904abc65c916f77a
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-26 22:21:43 -0700

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,12 @@
+# demo pop --> Procedure oriented programming
+
+def f1():
+	print("welcome to lect 1")
+	print("introduction")
+	print("installation")
+
+f1()
+f1()
+f1()
+
+#indentation (syntax in python)- consistent use of tabs and spaces in indentation
