257db4a7a96e7e7fb884607b11e90b41c44e113e
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-26 22:18:05 -0700

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,13 @@
+#wapp to accept as input three integers and add them
+
+print("enter first integer ")
+s1 = input()
+n1 = int(s1)
+
+s2 = input("enter second integer ")
+n2 = int(s2)
+
+n3 = int(input("enter third integer "))
+
+ans = n1 + n2 + n3
+print("ans of adding 3 integers = ", ans )
