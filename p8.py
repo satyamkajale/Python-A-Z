f97745ab8952ae035921203412fecda7a2cc2091
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-27 19:57:18 -0700

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,9 @@
+# wapf to add two integers
+
+def add(n1, n2):
+	res = n1 + n2
+	print("res = ",res)
+
+n1 = int(input("enter first integer "))
+n2 = int(input("enter second integer "))
+add(n1, n2)
