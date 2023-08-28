324fd19a851122a2ec8e0af5d485b1a69aa681a8
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-27 20:04:09 -0700

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,21 @@
+# demo of different data types
+
+a = 10
+print(a, type(a))
+
+a = 2.3
+print(a, type(a))
+
+a = "John"
+print(a, type(a))
+
+a = 'wick'
+print(a, type(a))
+
+a = True
+print(a, type(a))
+
+a = False
+print(a, type(a))
+
+# type() --> returns the data type of the variable
