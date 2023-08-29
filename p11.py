361acb2d277cfdcb841a0a294a5558277bd99f26
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-28 22:02:17 -0700

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,12 @@
+# wapp to read two numbers and exchange / swap their values
+
+n1 = float(input("enter first number "))
+n2 = float(input("enter second number "))
+
+print("n1 = ", n1, " n2 = ", n2)
+
+n1 , n2 = n2 , n1
+
+print("n1 = ", n1, " n2 = ", n2)
+
+#LOC --> Lines of code
