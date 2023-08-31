e49fb64c0c09e99fc21bf065780e6e26c07c658e
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-30 22:05:14 -0700

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,13 @@
+#wapp to read two integers and add them
+
+And = int(input("enter first integer "))
+oR = int(input("enter second integer "))
+ELSE = And + oR
+print(ELSE)
+
+_ = int(input("enter first integer "))
+__ = int(input("enter second integer "))
+___ = _ + __
+print(___)
+
+#BAD PROGRAMMING PRACTICE
