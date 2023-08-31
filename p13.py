cee969aca3707ca4e2d902c4c0767eb1b8dd0dce
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-30 22:18:21 -0700

    Add files via upload

---
+++ p13.py
@@ -0,0 +1,13 @@
+# wapp to read amount in dollars and convert into rupees
+# if possible then print currency symbol
+
+
+dollars = float(input("enter amount in dollars "))
+rupees = dollars * 72.53
+
+print("dollars = \u0024", dollars)
+print("rupees = \u20B9", rupees)
+
+
+print("dollars = \u0024", round(dollars,2))
+print("rupees = \u20B9", round(rupees,2))
