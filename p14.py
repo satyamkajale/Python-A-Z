d42a30281230c985675cb02cad9281e5a12ecba7
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-08-30 22:39:44 -0700

    Add files via upload

---
+++ p14.py
@@ -0,0 +1,10 @@
+# wapp to read temperature in farenheit and convert into celcius
+
+far = float(input("enter amount in far "))
+cel = (far - 32) * 5/9
+
+print("far = ", far)
+print("cel = ", cel)
+
+print("far = ", round(far,2))
+print("cel = ", round(cel,3), "\u00B0")
