81b7a94b4ad3f340170b4f36fa51f32f11fc38d1
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-01 22:54:32 -0700

    Add files via upload

---
+++ p16.py
@@ -0,0 +1,16 @@
+# wapp to read three integers and find their sum and product
+
+print("enter first integer ")
+s1 = input()
+n1 = int(s1)
+
+s2 = input("enter second integer ")
+n2 = int(s2)
+
+n3 = int(input("enter third integer "))
+
+sum = n1 + n2 + n3
+product = n1 * n2 * n3
+
+print("sum = ", sum)
+print("product = ", product)
