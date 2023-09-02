8e2309f158b9dbada97ee657a03a98da4f030986
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-01 23:00:20 -0700

    Add files via upload

---
+++ p17.py
@@ -0,0 +1,15 @@
+# wapp to read length and breadth of rect 
+# find and print area and perimeter
+
+length = float(input("enter length "))
+breadth = float(input("enter breadth "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print("area = ", area)
+print("perimeter = ", perimeter)
+
+print("area = ", round(area,2))
+print("perimeter = ", round(perimeter,2))
+
