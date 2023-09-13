07fada23d72fc9fcd0ba271bde103af491baa00b
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-12 22:10:48 -0700

    Update p20.py

--- p20.py
+++ p20.py
@@ -1,4 +1,4 @@
-# wapp to read marks between 0 and 100 and print the grade
+# Python Program to read marks between 0 and 100 and print the grade
 # marks >=80 print Grade A, marks >= 60 print Grade B
 # marks >=40 print Grade C else print Grade D
 
