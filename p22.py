18839254aa2a0243d71ea9c1fa774c28d7e703dc
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-14 22:22:32 -0700

    Add files via upload

---
+++ p22.py
@@ -0,0 +1,14 @@
+# wapp to find the sum of first "n" +ve integers
+# i/p:5 1+2+3+4+5 = 15
+
+num = int(input("enter +ve integer "))
+
+#approach 1
+if num < 0:
+	print("invalid input")
+else:
+	sum , i = 0 , 1
+	while i <= num:
+		sum += i
+		i += 1
+	print("sum = ", sum)
