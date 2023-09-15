1f0ca5b705b04647345f7e15808cb1ea0db9334f
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-14 22:22:44 -0700

    Add files via upload

---
+++ p23.py
@@ -0,0 +1,13 @@
+# wapp to read an integer and find the sum of digits
+# i/p: 154  1 + 5 + 4 = 10
+
+num = int(input("enter an integer "))
+if num < 0:
+	print("invalid input")
+else:
+	sum = 0
+	while num > 0:
+		digit = num % 10
+		sum = sum + digit
+		num = num // 10
+	print("sum = ", sum)
