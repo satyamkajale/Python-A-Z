211b681dd49d97c2bae758ec7266b9ca2bfc3020
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-14 22:21:54 -0700

    Add files via upload

---
+++ p21.py
@@ -0,0 +1,25 @@
+# wapp to find the sum of first "n" +ve integers
+# i/p:5 1+2+3+4+5 = 15
+
+num = int(input("enter +ve integer "))
+
+#approach 1
+if num < 0:
+	print("invalid input")
+else:
+	sum = 0
+	i = 1
+	while i <= num:
+		sum = sum + i
+		i = i + 1
+	print("sum = ", sum)
+
+#Approach 2
+
+ans = (num * (num + 1) / 2)
+print("sum = ", sum)
+
+
+
+
+
