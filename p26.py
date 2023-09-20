e930236cca349fda89cca59e1e92a820bd48e9fe
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-19 23:38:23 -0700

    Add files via upload

---
+++ p26.py
@@ -0,0 +1,14 @@
+#wapp to read +ve integer and find the reverse
+# 8732   2378
+
+num = int(input("enter a +ve integer "))
+if num < 0:
+	print("Invalid input")
+else:
+	print("num = ", num)
+	rev = 0
+	while num > 0:
+		digit = num % 10
+		rev = rev * 10 + digit
+		num = num//10
+	print("rev = ", rev)
