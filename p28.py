d5909e93a70dc38c7d10376fce83ff45ad6dc947
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-09-25 23:18:05 -0700

    Update p28.py

--- p28.py
+++ p28.py
@@ -1,4 +1,4 @@
-# python program to find the factorial of the given number
+# Python program to find the factorial of the given number
 # 0! = 1	1! = 1	 	2! = 2	  3! = 6
 
 num = int(input("enter the integer "))
