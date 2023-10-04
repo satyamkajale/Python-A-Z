940475b857fbc1681bdba7e8f8f6dfc572c81133
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-10-03 22:29:06 -0700

    Add files via upload

---
+++ p34.py
@@ -0,0 +1,10 @@
+# wapp to read username and password
+# if username is kamal and password is classes then o/p success else failure
+
+username = input("enter username ")
+password = input("enter password ")
+
+if (username == "kamal") and (password == "classes"):
+	print("success")
+else:
+	print("failure")
