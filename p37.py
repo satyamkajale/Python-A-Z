8f2fa2a5abdb9d678bfeb6b69444d4e95c1d5ab8
satyamkajale <66799948+satyamkajale@users.noreply.github.com>
2023-10-07 23:16:39 -0700

    Add files via upload

---
+++ p37.py
@@ -0,0 +1,10 @@
+# wapp to read and sort the string in alphabetical order
+
+s1 = input("enter a string ")
+print(s1)
+
+d1 = sorted(s1)
+print(d1)
+
+ns1 = "".join(d1)	#Join all items into a string
+print(ns1)
